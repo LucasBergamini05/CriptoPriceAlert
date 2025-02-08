6927c6d505fffdeceebf374d1e77e3f2bd2811c7
Lucas Bergamini <lucas.bergamini05@outlook.com>
2025-02-08 00:35:49 -0300

    Aprimorando alerta para verificar banda superior

--- alert.py
+++ alert.py
@@ -17,9 +17,11 @@ SMTP_PORT = 587
 
 EMAIL_TO = os.getenv("EMAIL_TO")
 
+# URL da API da Binance 
+# (No GitHub, é binance.us, mas, localmente, é binance.com)
 BINANCE_API_LINK = os.getenv("BINANCE_API_LINK")
 
-# Obtém os top 100 ativos da CoinGecko
+# Obtém os top 50 ativos da CoinGecko
 def get_top_100_symbols():
     url = "https://api.coingecko.com/api/v3/coins/markets"
     params = {"vs_currency": "usd", "order": "market_cap_desc", "per_page": 50, "page": 1}
@@ -55,12 +57,14 @@ def check_bollinger_breach(symbol):
     # Calcula as Bandas de Bollinger (20 períodos, 3 desvios padrão)
     indicator_bb = ta.volatility.BollingerBands(close=df["close"], window=20, window_dev=3)
     df["bb_lower"] = indicator_bb.bollinger_lband()
+    df["bb_upper"] = indicator_bb.bollinger_hband()
     
     # Verifica se o preço atual está abaixo da banda inferior
     last_close = df["close"].iloc[-1]
     last_bb_lower = df["bb_lower"].iloc[-1]
+    last_bb_upper = df["bb_upper"].iloc[-1]
     
-    return last_close < last_bb_lower
+    return last_close < last_bb_lower or last_close > last_bb_upper
 
 # Envia e-mail com os ativos que romperam a banda inferior
 def send_email(assets):
@@ -68,7 +72,7 @@ def send_email(assets):
         return
     
     subject = "⚠️ Alerta de Criptomoeda - Banda de Bollinger"
-    body = f"As seguintes criptomoedas estão abaixo da banda de Bollinger de 3 desvios:\n\n" + "\n".join(assets)
+    body = f"As seguintes criptomoedas estão fora das bandas de Bollinger de 3 desvios:\n\n" + "\n".join(assets)
     
     msg = MIMEText(body)
     msg["Subject"] = subject
@@ -91,10 +95,10 @@ def main():
     breached_assets = [symbol for symbol in symbols if check_bollinger_breach(symbol)]
     
     if breached_assets:
-        print("⚠️ Ativos abaixo da banda inferior:", breached_assets)
+        print("⚠️ Ativos fora das bandas Bollinger:", breached_assets)
         send_email(breached_assets)
     else:
-        print("✅ Nenhum ativo rompeu a banda inferior.")
+        print("✅ Nenhum ativo fora das bandas de Bollinger.")
 
 # Executa o script
 if __name__ == "__main__":
