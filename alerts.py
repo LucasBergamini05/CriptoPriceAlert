e956d6c64b8156bec56d898672f44b11f1fd45f7
Lucas Bergamini <lucas.bergamini05@outlook.com>
2025-02-09 20:27:23 -0300

    Refatorando alertas.py Esse commit organiza as variáveis do arquivo

--- alerts.py
+++ alerts.py
@@ -18,18 +18,20 @@ def handle_bollinger_alert(df: pd.DataFrame):
     # Verifica os ativos
     symbols_notes = {}
     for symbol, settings in bollinger_settings.items():
-        df = get_klines(symbol, interval=settings["interval"], limit=settings["window"])
-        if df is None:
+        interval, window, window_dev = (settings.get(k) for k in ("interval", "window", "window_dev"))
+
+        klines_df = get_klines(symbol, interval=interval, limit=window)
+        if klines_df is None:
             continue
         
-        result = check_bollinger_breach(df, window=settings["window"], window_dev=settings["window_dev"])
+        result = check_bollinger_breach(klines_df, window=window, window_dev=window_dev)
 
         if result == -1:
-            symbols_notes[symbol] = f"Preço abaixo da Banda Inferior de Bollinger {settings["interval"]} ↘️"
+            symbols_notes[symbol] = f"Preço abaixo da Banda Inferior de Bollinger {interval} ↘️"
             continue
 
         if result == 1:
-            symbols_notes[symbol] = f"Preço acima da Banda Superior de Bollinger {settings["interval"]} ↗️"
+            symbols_notes[symbol] = f"Preço acima da Banda Superior de Bollinger {interval} ↗️"
       
     return symbols_notes
 
@@ -47,17 +49,19 @@ def handle_volume_alert(df: pd.DataFrame):
     # Verifica os ativos
     symbols_notes = {}
     for symbol, settings in volume_settings.items():
-        df = get_klines(symbol, interval=settings["interval"], limit=settings["window"])
-        if df is None:
+        interval, window = (settings.get(k) for k in ("interval", "window"))
+
+        klines_df = get_klines(symbol, interval=interval, limit=window)
+        if klines_df is None:
             continue
         
-        result = calculate_sma(df["volume"], window=settings["window"])
+        result = calculate_sma(klines_df["volume"], window=window)
 
-        last_volume = float(df["volume"].iloc[-1])
+        last_volume = float(klines_df["volume"].iloc[-1])
         last_volume_median = float(result.iloc[-1])
 
         if last_volume > last_volume_median:
-            symbols_notes[symbol] = f"Volume acima da média ({settings["interval"]}) ↗️"
+            symbols_notes[symbol] = f"Volume acima da média ({interval}) ↗️"
 
     return symbols_notes
 
@@ -67,27 +71,25 @@ def handle_current_price_alert(df: pd.DataFrame):
     df = prepare_dataframe(df, {
         "interval": ("Tempo Gráfico", "4h", str),
         "price": ("Valor", None, float),
-        "type": ("Tipo", ">", str)
+        "comparison": ("Comparação", ">", str)
     })
 
     # Cria um dicionário com as configurações
-    price_settings = df.set_index("Ativo")[["interval", "price", "type"]].to_dict(orient="index")
+    price_settings = df.set_index("Ativo")[["interval", "price", "comparison"]].to_dict(orient="index")
 
     # Verifica os ativos
     symbols_notes = {}
     for symbol, settings in price_settings.items():
-        price = settings["price"]
-        type = settings["type"]
-        interval = settings["interval"]
+        price, comparison, interval = (settings.get(k) for k in ("price", "comparison", "interval"))
 
-        df = get_klines(symbol, interval=interval, limit=1)
-        if df is None:
+        klines_df = get_klines(symbol, interval=interval, limit=1)
+        if klines_df is None:
             continue
 
-        last_price = float(df["close"].iloc[-1])
+        last_price = float(klines_df["close"].iloc[-1])
 
-        if compare_values(last_price, price, type):
-            symbols_notes[symbol] = get_message_map(price, type, interval)
+        if compare_values(last_price, price, comparison):
+            symbols_notes[symbol] = get_message_map(price, comparison, interval)
 
     return symbols_notes
 
@@ -107,21 +109,18 @@ def handle_moving_average_alert(df: pd.DataFrame):
     # Verifica os ativos
     symbols_notes = {}
     for symbol, settings in ma_settings.items():
-        type = settings["type"]
-        comparison = settings["comparison"]
-        window = settings["window"]
-        interval = settings["interval"]
+        type, comparison, window, interval = (settings.get(k) for k in ("type", "comparison", "window", "interval"))
 
-        df = get_klines(symbol, interval=interval, limit=window)
-        if df is None:
+        klines_df = get_klines(symbol, interval=interval, limit=window)
+        if klines_df is None:
             continue
 
         if type == "SMA":
-            result = calculate_sma(df["close"], window=window)
+            result = calculate_sma(klines_df["close"], window=window)
         else:
-            result = calculate_ema(df["close"], window=window)
+            result = calculate_ema(klines_df["close"], window=window)
 
-        last_price = float(df["close"].iloc[-1])
+        last_price = float(klines_df["close"].iloc[-1])
         last_ma = float(result.iloc[-1])
 
         ma_text = f"{type} de {window} períodos"
