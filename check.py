91627cefba514abb5a2e019b4130aca8f41baa41
Lucas Bergamini <lucas.bergamini05@outlook.com>
2025-02-09 12:26:54 -0300

    Criando tratamento de erro em check.py

--- check.py
+++ check.py
@@ -5,20 +5,24 @@ from fetch import get_settings
 def check():
     notes = {}
     for alert_name, alert_func in ALERTS_MAP.items():
-        # Pega as configurações da aba correspondente
-        df = get_settings(alert_name)
+        try:
+            # Pega as configurações da aba correspondente
+            df = get_settings(alert_name)
 
-        if(df is None):
-            print(f"Erro ao buscar configurações para {alert_name}")
-            continue
+            if(df is None):
+                print(f"Erro ao buscar configurações para {alert_name}")
+                continue
 
-        # Chama a função do alerta com os dados obtidos
-        alert_note = alert_func(df)
+            # Chama a função do alerta com os dados obtidos
+            alert_note = alert_func(df)
 
-        # Adiciona as notas ao dicionário
-        for symbol, symbol_notes in alert_note.items():
-            if symbol not in notes:
-                notes[symbol] = []
-            notes[symbol].append(symbol_notes)
+            # Adiciona as notas ao dicionário
+            for symbol, symbol_notes in alert_note.items():
+                if symbol not in notes:
+                    notes[symbol] = []
+                notes[symbol].append(symbol_notes)
+
+        except Exception as e:
+            print(f"⚠️ Erro ao verificar alerta {alert_name}: {e}")
 
     return notes
